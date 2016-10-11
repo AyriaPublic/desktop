b874358aa479390885fdf5628be7cb1860dd4ce5
Selwyn <talk@selwyn.cc>
2016-10-11 21:33:22 +0200

    Cache already downloaded steamapps
    * Cache already downloaded steamapps
    
    Add caching capabilities, this require a lot changes so some parts are
    refactored to make them compatible with this change.
    
    Solves #7
    
    * Refactor cache directory error handling

--- src/list-steamapps/list-steamapps.js
+++ src/list-steamapps/list-steamapps.js
@@ -1,17 +1,23 @@
 'use strict';
 
 const pify = require('pify');
-const fs = pify(require('fs'));
+const fs = pify(require('fs'), {exclude: ['createWriteStream']});
 const path = require('path');
 
 const entries = require('object.entries');
 const envPaths = require('env-paths');
+const flatCache = require('flat-cache');
 const hyperquest = require('hyperquest');
 const JSONStream = require('JSONStream');
+const R = require('ramda');
 const vdf = require('vdfjs');
 const winreg = require('winreg');
 
+const cachePath = envPaths('ayria', {suffix: ''}).cache;
+const steamappsCache = flatCache.load('steamapps', cachePath);
+
 // Return location of primary Steam library (installation directory)
+// getSteamInstallPath :: () -> Promise -> String
 const getSteamInstallPath = function () {
     if (process.platform === 'linux') {
         return Promise.resolve(envPaths('Steam', {suffix: ''}).data);
@@ -34,6 +40,7 @@ const getSteamInstallPath = function () {
 };
 
 // Parses the `libraryfolders.vdf` config as found in the primary Steam library's steamapps directory
+// getSteamLibraryFoldersConfig :: () -> Promise -> {Object}
 const getSteamLibraryFoldersConfig = function () {
     return getSteamInstallPath().then(steamInstall => {
         const config = path.join(steamInstall, 'steamapps', 'libraryfolders.vdf');
@@ -43,6 +50,7 @@ const getSteamLibraryFoldersConfig = function () {
 };
 
 // Return location of all Steam libraries, including the installation directory
+// getSteamLibraries :: () -> Promise -> [String]
 const getSteamLibraries = function () {
     return Promise.all([getSteamInstallPath(), getSteamLibraryFoldersConfig()])
         .then(([steamInstall, settings]) => {
@@ -55,14 +63,16 @@ const getSteamLibraries = function () {
 };
 
 // Return each Steam libraries' steamapps directory
-const getSteamAppsPaths = function () {
+// getSteamappsPaths :: () -> [String]
+const getSteamappsPaths = function () {
     return getSteamLibraries().then(libraries => libraries.map(library => {
         return path.join(library, 'steamapps');
     }));
 };
 
-// Return array of app ID's
-const getSteamAppIds = function (path) {
+// Return array of app ID's reading from files in the give given path
+// getSteamappId :: String -> Promise -> [String]
+const getSteamappId = function (path) {
     return fs.readdir(path).then(function (fileNames) {
         // Filter out non appmanifest fileNames
         fileNames = fileNames.filter(function (fileName) {
@@ -76,30 +86,30 @@ const getSteamAppIds = function (path) {
     });
 };
 
-// Return array of promises containing app information
-const getSteamAppInfo = function (appIds) {
+// Requests appInfo from the passed appId at the Steam API
+// getSteamappInfo :: String -> Promise -> Object
+const getSteamappInfo = function (appId) {
     const steamApiUrl = 'http://store.steampowered.com/api/appdetails?appids=';
 
-    return Promise.all(appIds.map(function (appId) {
-        return new Promise(function (resolve, reject) {
-            hyperquest(`${steamApiUrl}${appId}&filters=basic,background`)
-              .pipe(JSONStream.parse('*'))
-              .on('data', function (response) {
-                  if (response.success) {
-                      renderSteamApp(response.data);
-                      resolve(response.data);
-                  }
-                  reject('Steam API failed at responding.');
-              })
-              .on('error', function (error) {
-                  reject(error);
-              });
-        });
-    }));
+    return new Promise(function (resolve, reject) {
+        hyperquest(`${steamApiUrl}${appId}&filters=basic,background`)
+          .pipe(JSONStream.parse('*'))
+          .on('data', function (response) {
+              // if response.data.background
+              if (response.success) {
+                  resolve(response.data);
+              }
+              reject('Steam API failed at responding.');
+          })
+          .on('error', function (error) {
+              reject(error);
+          });
+    });
 };
 
 // Render passed object appData
-const renderSteamApp = function (appData) {
+// renderSteamapp :: Object -> ()
+const renderSteamapp = function (appData) {
     const gamesListElement = document.querySelector('[data-list-games]');
 
     const appItem = document.createElement('li');
@@ -118,6 +128,58 @@ const renderSteamApp = function (appData) {
     gamesListElement.appendChild(appItem);
 };
 
-getSteamAppsPaths()
-    .then(paths => Promise.all(paths.map(getSteamAppIds)))
-    .then(appIds => appIds.forEach(getSteamAppInfo));
+// Takes steamappData, downloads the background and save the data to the cache
+// cacheSteamappData :: Object -> Promise -> Object
+const cacheSteamappData = function (appData) {
+    const backgroundPath = path.join(cachePath, 'backgrounds');
+    const appBackgroundPath = path.format({
+        dir: backgroundPath,
+        name: appData.steam_appid,
+        ext: '.jpg'
+    });
+
+    steamappsCache.setKey(appData.steam_appid, appData);
+
+    return new Promise(function (resolve, reject) {
+        hyperquest(appData.background)
+            .pipe(fs.createWriteStream(appBackgroundPath, {}))
+            .on('finish', function () {
+                appData.background = appBackgroundPath;
+                resolve(appData);
+            })
+            .on('error', function (error) {
+                // If directory doesn't exist create it and call again
+                if (error.code === 'ENOENT') {
+                    return fs.mkdir(cachePath)
+                        .then(() => fs.mkdir(backgroundPath))
+                        .then(() => resolve(cacheSteamappData(appData)))
+                        .catch(function (error) {
+                            // If directory already exists call again
+                            if (error.code === 'EEXIST') {
+                                resolve(cacheSteamappData(appData));
+                            } else {
+                                reject(error);
+                            }
+                        });
+                } else {
+                    reject(error);
+                }
+            });
+    });
+};
+
+getSteamappsPaths()
+    .then(paths => Promise.all(paths.map(getSteamappId)))
+    .then(R.unnest)
+    .then(R.map(R.either(
+        R.bind(steamappsCache.getKey, steamappsCache),
+        R.pipeP(getSteamappInfo, cacheSteamappData)
+    )))
+    .then(R.forEach(function (appData) {
+        Promise.resolve(appData).then(function (appData) {
+            renderSteamapp(appData);
+        });
+    }))
+    .then(steamapps => Promise.all(steamapps).then(() => {
+        steamappsCache.save();
+    }));
