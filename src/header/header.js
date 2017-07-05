ddc94c1c2a1079454b7cfe2dadecd6206fe66ead
Selwyn <Siilwyn@users.noreply.github.com>
2017-07-05 16:15:55 +0200

    Add initial local plugin installation

--- src/header/header.js
+++ src/header/header.js
@@ -1,4 +1,6 @@
 'use strict';
+const dialog = require('electron').remote.dialog;
+const extractPlugin = require('../add-plugin/add-plugin').extractPlugin;
 
 const navigation = {
     previous: document.querySelector('[data-header-previous]'),
@@ -22,10 +24,28 @@ navigation.previous.addEventListener('click', function (event) {
     );
 });
 
+navigation.addPlugin.addEventListener('click', function (event) {
+    event.preventDefault();
+    dialog.showOpenDialog(
+        {
+            'title': 'Open Ayria plugin package',
+            'filters': [
+                {'name': 'Ayria plugin package', extensions: ['zip']}
+            ]
+        },
+        function (filePath) {
+            if (!filePath) return;
+
+            extractPlugin(
+                filePath[0],
+                navigation.addPlugin.getAttribute('game-slug')
+            );
+        }
+    );
+});
+
 const renderHeader = function (state) {
-    if(!state.headerNavigation) {
-        return;
-    }
+    if (!state.headerNavigation) return;
 
     Object.entries(navigation).forEach(function ([key, element]) {
         element.setAttribute('disabled', !state.headerNavigation[key]);
@@ -33,5 +53,8 @@ const renderHeader = function (state) {
 };
 
 module.exports = {
-    render: renderHeader,
+    render: (state) => {
+        renderHeader(state);
+        navigation.addPlugin.setAttribute('game-slug', state.appSlug);
+    },
 };
