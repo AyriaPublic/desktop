dddfe53e5ab2cc8ce8c97a2288e6151446dabf84
Siilwyn <talk@selwyn.cc>
2017-11-09 16:30:26 +0100

    Add initial game launching

--- src/core/steam-fs.js
+++ src/core/steam-fs.js
@@ -1,24 +1,26 @@
 'use strict';
 const pify = require('pify');
 
+const arch = require('arch');
 const binaryVdf = require('binary-vdf');
 const envPaths = require('env-paths');
 const winreg = require('winreg');
 const vdf = require('vdfjs');
-const fs = pify(require('fs'), {exclude: ['createReadStream']});
+const fs = pify(require('fs'), { exclude: ['createReadStream'] });
 const path = require('path');
 const entries = require('object.entries');
+const R = require('ramda');
 
 // Return location of primary Steam library (installation directory)
 // getInstallPath :: () -> Promise -> String
 const getInstallPath = function () {
     if (process.platform === 'linux') {
-        return Promise.resolve(envPaths('Steam', {suffix: ''}).data);
+        return Promise.resolve(envPaths('Steam', { suffix: '' }).data);
     }
 
     const steamRegistryKey = winreg({
         hive: winreg.HKCU,
-        key: '\\SOFTWARE\\Valve\\Steam'
+        key: '\\SOFTWARE\\Valve\\Steam',
     });
 
     return new Promise(function (resolve, reject) {
@@ -36,7 +38,11 @@ const getInstallPath = function () {
 // getLibraryFoldersConfig :: () -> Promise -> {Object}
 const getLibraryFoldersConfig = function () {
     return getInstallPath().then(steamInstall => {
-        const configPath = path.join(steamInstall, 'steamapps', 'libraryfolders.vdf');
+        const configPath = path.join(
+            steamInstall,
+            'steamapps',
+            'libraryfolders.vdf'
+        );
 
         return fs.readFile(configPath, 'utf-8').then(vdf.parse);
     });
@@ -45,8 +51,8 @@ const getLibraryFoldersConfig = function () {
 // Return location of all Steam libraries, including the installation directory
 // getSteamLibraries :: () -> Promise -> [String]
 const getSteamLibraries = function () {
-    return Promise.all([getInstallPath(), getLibraryFoldersConfig()])
-        .then(([steamInstall, settings]) => {
+    return Promise.all([getInstallPath(), getLibraryFoldersConfig()]).then(
+        ([steamInstall, settings]) => {
             const folders = entries(settings['LibraryFolders'])
                 // Filter any non-numeric entries by matching the key
                 .filter(pair => pair[0].match(/^\d+$/))
@@ -54,19 +60,22 @@ const getSteamLibraries = function () {
                 .map(pair => pair[1]);
 
             return [steamInstall].concat(folders);
-        });
+        }
+    );
 };
 
 // Return each Steam libraries' steamapps directory
 // getSteamappsDirectories :: () -> [String]
 const getSteamappsDirectories = function () {
-    return getSteamLibraries().then(libraries => libraries.map(library => {
-        return path.join(library, 'steamapps');
-    }));
+    return getSteamLibraries().then(libraries =>
+        libraries.map(library => {
+            return path.join(library, 'steamapps');
+        })
+    );
 };
 
 // Return array of app ID's reading from files in the give given path
-// getSteamappIds :: String -> Promise -> [String]
+// getSteamappIds :: String -> Promise -> [Number]
 const getSteamappIds = function (path) {
     return fs.readdir(path).then(function (fileNames) {
         // Filter out non appmanifest fileNames
@@ -76,26 +85,100 @@ const getSteamappIds = function (path) {
 
         // Get the digits and thus app ID's out of the filename
         return fileNames.map(function (fileName) {
-            return fileName.match(/\d+/)[0];
+            return Number(fileName.match(/\d+/)[0]);
         });
     });
 };
 
 // Get steamapp info from appinfo.vdf file
 // getAppInfo :: Number -> Promise -> Object
-const getSteamappInfo = function (appId) {
-    return getInstallPath()
-        .then((installPath) => {
-            const vdfPath = path.join(installPath, 'appcache', 'appinfo.vdf');
-            return binaryVdf.readAppInfo(fs.createReadStream(vdfPath))
-                .then(appinfo => appinfo.find((app) => app.id === appId))
-                .then(app => app.entries);
-        });
+const getAppInfo = function (appId) {
+    return getInstallPath().then(installPath => {
+        const vdfPath = path.join(installPath, 'appcache', 'appinfo.vdf');
+        return binaryVdf
+            .readAppInfo(fs.createReadStream(vdfPath))
+            .then(appinfo => appinfo.find(app => app.id === appId))
+            .then(app => app.entries);
+    });
+};
+
+// Transform Node.js platform to Steam platform naming
+// getSteamPlatform :: String -> String
+const getSteamPlatform = function (platform) {
+    switch (platform) {
+    case 'darwin':
+        return 'macos';
+    case 'win32':
+        return 'windows';
+    case 'linux':
+        return 'linux';
+    }
+};
+
+// Transform arch string with x notation to number
+// getSteamArch :: String -> Number
+const getSteamArch = function (arch) {
+    return Number(arch.slice(1));
+};
+
+// Get the appropiate OS launchconfig
+// getSteamappVdfLaunch :: Object -> Object
+const getSteamappVdfLaunch = function ({
+    steamPlatform = getSteamPlatform(process.platform),
+    steamArch = getSteamArch(arch()),
+    launchConfig,
+}) {
+    return R.pipe(
+        R.values,
+        R.either(
+            R.ifElse(R.find(R.prop('config')), R.F, () => launchConfig[0]),
+            R.either(
+                R.find(
+                    R.both(
+                        R.pathEq(['config', 'oslist'], steamPlatform),
+                        R.pathEq(['config', 'osarch'], steamArch)
+                    )
+                ),
+                R.find(R.pathEq(['config', 'oslist'], steamPlatform))
+            )
+        )
+    )(launchConfig);
+};
+
+// Get the directory a steamapp is located in
+// getSteamappLibraryDir :: String -> Promise -> String
+const getSteamappLibraryDir = function (appId) {
+    return getSteamappsDirectories().then(paths => {
+        return Promise.all(
+            paths.map(path => {
+                return getSteamappIds(path).then(appIds => ({
+                    path,
+                    hasApp: appIds.includes(appId),
+                }));
+            })
+        )
+            .then(paths => paths.filter(({ hasApp }) => hasApp))
+            .then(viablePaths => {
+                if (viablePaths.length === 1) {
+                    return viablePaths[0].path;
+                } else if (viablePaths.length > 1) {
+                    return Promise.reject(
+                        `The steamapp ${appId} was found in multiple libraries`
+                    );
+                } else {
+                    return Promise.reject(
+                        `The steamapp ${appId} was not found in any libraries`
+                    );
+                }
+            });
+    });
 };
 
 module.exports = {
     getSteamLibraries,
     getSteamappsDirectories,
     getSteamappIds,
-    getSteamappInfo,
+    getAppInfo,
+    getSteamappLibraryDir,
+    getSteamappVdfLaunch,
 };
--- src/partials/game-detail/game-detail.js
+++ src/partials/game-detail/game-detail.js
@@ -2,19 +2,20 @@
 const R = require('ramda');
 
 const { pluginStore } = require('../../core/db');
+const { execFile } = require('child_process');
+const steamFs = require('../../core/steam-fs.js');
+const path = require('path');
 
 // Get the plugin files from the plugin store
 // getGamePlugins :: Object -> Object
-const getGamePlugins = function ({appid: gameId}) {
-    return pluginStore.query(
-        'plugin-index/byGameId',
-        {
+const getGamePlugins = function ({ appid: gameId }) {
+    return pluginStore
+        .query('plugin-index/byGameId', {
             key: `steam:${gameId}`,
             include_docs: true,
-        }
-    )
+        })
         .then(R.prop('rows'))
-        .then(R.map(R.prop('doc')))
+        .then(R.map(R.prop('doc')));
 };
 
 // Add plugin information to the DOM plugin list
@@ -31,10 +32,47 @@ const renderPlugin = function (pluginData) {
     pluginList.appendChild(pluginItem);
 };
 
+// Render button to launch game
+// renderLaunchOption :: String, Object -> ()
+const renderLaunchOption = function (execDir, launchOption) {
+    const launchOptions = document.querySelector('[data-launch-options]');
+    const launchOptionButton = document.createElement('button');
+    launchOptions.innerHTML = '';
+
+    // Fill in DOM nodes with data
+    launchOptionButton.textContent = `Launch ${launchOption['executable']}`;
+    launchOptionButton.addEventListener('click', () => {
+        const execArgs = launchOption['arguments']
+            ? launchOption['arguments'].split(' ')
+            : [];
+
+        const bootstrapDir = path.join(process.cwd(), 'src', 'bootstrap');
+        const bootstrapPath = path.join(bootstrapDir, 'Bootstrap64.exe');
+        const bootstrapArgs = [execDir, launchOption['executable'], ...execArgs];
+
+        const bootstrapProcess = execFile(
+            bootstrapPath,
+            bootstrapArgs,
+            { cwd: bootstrapDir },
+            error => {
+                if (error) throw error;
+            }
+        );
+
+        bootstrapProcess.stdout.pipe(process.stdout);
+        bootstrapProcess.stderr.pipe(process.stderr);
+    });
+
+    // Construct and insert DOM structure
+    launchOptions.appendChild(launchOptionButton);
+};
+
 // Render passed game slug and data
 // renderGameDetail :: Object -> ()
 const renderGameDetail = function (gameData) {
-    const gameBackground = document.querySelector('[data-game-detail-background]');
+    const gameBackground = document.querySelector(
+        '[data-game-detail-background]'
+    );
     const gameName = document.querySelector('[data-game-detail-name]');
     const pluginList = document.querySelector('[data-plugin-list]');
 
@@ -43,8 +81,14 @@ const renderGameDetail = function (gameData) {
 
     pluginList.innerHTML = '';
 
-    getGamePlugins(gameData)
-        .then(R.map(renderPlugin));
+    steamFs.getSteamappLibraryDir(gameData.appid).then(libraryDir => {
+        renderLaunchOption(
+            path.join(libraryDir, 'common', gameData.installDirectory),
+            steamFs.getSteamappVdfLaunch({ launchConfig: gameData.launch })
+        );
+    });
+
+    getGamePlugins(gameData).then(R.map(renderPlugin));
 };
 
 module.exports = {
