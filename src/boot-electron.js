f94e3ff2bbe5b48f8e859c6862557ad6f6547abb
Selwyn <talk@selwyn.cc>
2018-02-15 15:30:20 +0100

    Define and apply new main colors

--- src/boot-electron.js
+++ src/boot-electron.js
@@ -22,8 +22,9 @@ if (isDevelopment) {
 const createWindow = function () {
     mainWindow = new BrowserWindow({
         show: false,
-        minWidth: 500,
-        minHeight: 500
+        minWidth: 896,
+        minHeight: 504,
+        autoHideMenuBar: true,
     });
     mainWindow.loadURL(`file://${__dirname}/index.html`);
 
@@ -43,17 +44,4 @@ const createWindow = function () {
 
 app.on('ready', createWindow);
 
-// Quit when all windows are closed except on OS X
-app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
-
-app.on('activate', function () {
-    // On OS X it's common to re-create a window in the app when the
-    // dock icon is clicked and there are no other windows open
-    if (mainWindow === null) {
-        createWindow();
-    }
-});
+app.on('window-all-closed', app.quit);
